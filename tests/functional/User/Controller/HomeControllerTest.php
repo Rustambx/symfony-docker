ffc6ff700f7ec3cba8b1a129e447d31754c1eb0f
Rustambx <rustambx@gmail.com>
2021-01-31 17:48:33 +0500

    Created Register User tests

--- tests/functional/User/Controller/HomeControllerTest.php
+++ tests/functional/User/Controller/HomeControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace App\Tests\Functional\User\Controller;
 
-use App\Tests\Functional\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeControllerTest extends WebTestCase
 {
@@ -10,5 +12,30 @@ class HomeControllerTest extends WebTestCase
     {
         $client = $this->createClientForAdmin();
         $this->assertEquals('Hello', $client->getCrawler()->filter('html h1')->first()->text());
+    }
+
+    /**
+     * @return KernelBrowser
+     */
+    protected function createClientForAdmin(): KernelBrowser
+    {
+        static::ensureKernelShutdown();
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+        $this->assertEquals(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+
+        $client->followRedirect();
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+        $client->submitForm('Login', [
+            'email' => 'admin@mail.ru',
+            'password' => '12345678',
+        ]);
+        $crawler = $client->followRedirect();
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertEquals('Hello', $crawler->filter('h1')->first()->text());
+
+        return $client;
     }
 }
